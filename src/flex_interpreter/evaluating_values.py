c74c5265e53b19ba6e1a78dc5436f95390cc288a
Hassan220022 <hassansonson2002@gmail.com>
2025-04-27 21:12:21 +0300

    Update input request format and add function calls in test code

--- src/flex_interpreter/evaluating_values.py
+++ src/flex_interpreter/evaluating_values.py
@@ -122,7 +122,7 @@ def handle_numeric_value(value):
 
 def handle_scan_now(var_type, line_number, line_content, AI):
     if gv.web:
-      print("__INPUT_VALUE__")
+      print("__FLEX_INPUT_REQUEST__")
     
     value = input()
     try:
